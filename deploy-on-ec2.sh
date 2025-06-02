8f04549ca7ad558c30c9bf2db85296c69d63ecc7
aidabo <aidabo@janabiz.com>
2025-06-02 10:50:10 +0900

    Change image name of deploy

--- deploy-on-ec2.sh
+++ deploy-on-ec2.sh
@@ -5,7 +5,7 @@ S3_BUCKET="60-legacy"
 S3_FOLDER="deploy-image"
 AWS_REGION="ap-northeast-1"
 IMAGE_NAME="ghost"
-VERSION="5.115.1-alpine-c001"
+VERSION="5.116.2-alpine-next"
 
 # Download from S3
 aws s3 cp s3://$S3_BUCKET/$S3_FOLDER/$IMAGE_NAME-$VERSION.tar . --region $AWS_REGION
@@ -14,19 +14,19 @@ aws s3 cp s3://$S3_BUCKET/$S3_FOLDER/$IMAGE_NAME-$VERSION.tar . --region $AWS_RE
 docker compose down
 
 # remove old image and container
-# docker rmi $IMAGE_NAME:$VERSION --force 
+docker rmi $IMAGE_NAME:$VERSION --force 
 
 # Load Docker image
 docker load -i $IMAGE_NAME-$VERSION.tar
 
 # Run new container
-docker compose up -d --build
+docker compose up -d 
 
 # Cleanup
 rm $IMAGE_NAME-$VERSION.tar
 
 # Clean old image and and container
-# docker images | grep "<none>" | awk '{print $3}' | xargs docker rmi --force
+docker images | grep "<none>" | awk '{print $3}' | xargs docker rmi --force
 
 # Confirm images
 docker images
