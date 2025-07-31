67939a0f8afd18b2c568e37f8af1ba434fb2807a
aidabo <aidabo@janabiz.com>
2025-07-31 17:31:04 +0900

    change env to consistent with docker compose

--- env-dev.sh
+++ env-dev.sh
@@ -1,13 +1,25 @@
 # Docker compose environment variables defined in host
 
 # Development
-export GHOST_NODE_ENV=development
+# export GHOST_NODE_ENV=development
+# export GHOST_BACKEND_URL=http://localhost:2368/
+# export GHOST_FRONT_END_URL=http://localhost:3000/
+# export GHOST_MYSQL_USER=mysql
+# export GHOST_MYSQL_PASSWORD=mysql
+# export GHOST_MYSQL_DB=ghost_db
+# export GHOST_CONFIG_JSON=config.development.json 
+# export GHOST_DEBUG=true
+# export GHOST_LOG_LEVEL=debug
+# export GHOST_HOST_PORT=2368
+
+export GHOST_HOST_PORT=2368
 export GHOST_BACKEND_URL=http://localhost:2368/
 export GHOST_FRONT_END_URL=http://localhost:3000/
 export GHOST_MYSQL_USER=mysql
 export GHOST_MYSQL_PASSWORD=mysql
-export GHOST_MYSQL_DB=ghost_db
-export GHOST_CONFIG_JSON=config.development.json 
 export GHOST_DEBUG=true
 export GHOST_LOG_LEVEL=debug
-export GHOST_HOST_PORT=2368
+export GHOST_MYSQL_DB=ghost_db
+export GHOST_MYSQL_DB_TEST=ghost_db_test
+export GHOST_CONFIG_JSON=config.development.json
+export GHOST_NODE_ENV=development
